4e7c710d2dc8a6152dc729f02b91cde7736f812d
makar4eg1993 <s.makarov1993@gmail.com>
2020-01-10 23:03:26 +0300

    Init commit. Created database migration for next tables: players, tournaments.

---
+++ database/migrations/2020_01_10_195601_create_tournaments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTournamentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tournaments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',25);
+            $table->integer('price_pool');
+            $table->date('date_start')->nullable();
+            $table->date('date_finish')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tournaments');
+    }
+}
