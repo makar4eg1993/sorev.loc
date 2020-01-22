e4f67d490463094fd5b1ddd75ede9d83440e7151
makar4eg1993 <s.makarov1993@gmail.com>
2020-01-22 18:25:57 +0300

    tied games and matches

---
+++ database/migrations/2020_01_22_151852_update_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+
+            $table->bigInteger('match_id')->unsigned()->change();
+            $table->foreign('match_id')->references('id')->on('matches');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+
+            $table->dropForeign('match_id');
+            $table->integer('match_id')->change();
+        });  //
+    }
+}
